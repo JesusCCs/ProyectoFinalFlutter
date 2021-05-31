a30009b5296e2023338dd1db660e333cfd2a5a40
Jesús Cañas Cobos <jesus.c.cobos@gmail.com>
2021-05-31 22:53:23 +0200

    Se migra el login al nuevo sistema de formularios con libreria

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/localization/form_builder_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:mobile_app/pantallas/auth/login_screen.dart';
 
 import '_services/_base.dart';
@@ -13,6 +15,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Sweet',
+      supportedLocales: [
+        Locale('es')
+      ],
+      localizationsDelegates: [
+        FormBuilderLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       theme: new ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0)),
       home: new AuthenticationWrapper(),
     );
--- mobile_app/lib/pantallas/auth/login_screen.dart
+++ mobile_app/lib/pantallas/auth/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:mobile_app/_componentes/form_button.dart';
-import 'package:mobile_app/_componentes/input_field.dart';
 import 'package:mobile_app/_componentes/navigation_link.dart';
 import 'package:mobile_app/pantallas/auth/forgot_screen.dart';
 import 'package:mobile_app/pantallas/auth/register_screen.dart';
@@ -11,60 +11,90 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  late double screenHeight;
+  final _formKey = GlobalKey<FormBuilderState>();
 
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
+  String? _emailError;
+
+  submit() async {
+    final isValid = _formKey.currentState!.saveAndValidate();
+
+    if (!isValid) return;
+
+    setState(() => _emailError = null);
+
+    final email = _formKey.currentState!.fields['email']!.value;
+    final pass = _formKey.currentState!.fields['pass']!.value;
+
+    FocusScope.of(context).unfocus();
+  }
 
   @override
   Widget build(BuildContext context) {
-    screenHeight = MediaQuery.of(context).size.height;
+    double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ListView(
-          children: [
-            Padding(
-                padding: EdgeInsets.only(top: 50.0),
-                child: Text(
-                  "Bienvenido,",
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
+        child: FormBuilder(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.disabled,
+          child: ListView(
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(top: 50.0),
+                  child: Text(
+                    "Bienvenido,",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    "Inicia sesión para continuar",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black.withOpacity(.6),
+                    ),
+                  )),
+              SizedBox(height: screenHeight * .07),
+              FormBuilderTextField(
+                name: 'email',
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  errorText: _emailError
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.email(context),
+                  FormBuilderValidators.required(context),
+                  FormBuilderValidators.minLength(context, 3)
+                ])
+              ),
+              SizedBox(height: screenHeight * .05),
+              FormBuilderTextField(
+                name: 'pass',
+                  decoration: InputDecoration(
+                      labelText: 'Contraseña',
                   ),
-                )),
-            Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  "Inicia sesión para continuar",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black.withOpacity(.6),
-                  ),
-                )),
-            SizedBox(height: screenHeight * .07),
-            InputField(
-              labelText: "Email",
-              keyboardType: TextInputType.emailAddress,
-              controller: emailController
-            ),
-            SizedBox(height: screenHeight * .05),
-            InputField(
-              labelText: "Contraseña",
-              obscureText: true,
-              controller: passwordController
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: NavigationLink(
-                  text: '¿Olvidaste la contraseña?', screen: ForgotScreen()),
-            ),
-            SizedBox(height: screenHeight * .075),
-            FormButton(text: "Iniciar sesión"),
-            SizedBox(height: screenHeight * .05),
-            NavigationLink(text: 'Registrarme', screen: RegisterScreen())
-          ],
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(context),
+                  FormBuilderValidators.minLength(context, 5)
+                ])
+              ),
+              SizedBox(height: screenHeight * .015),
+              Align(
+                alignment: Alignment.centerRight,
+                child: NavigationLink(
+                    text: '¿Olvidaste la contraseña?', screen: ForgotScreen()),
+              ),
+              SizedBox(height: screenHeight * .075),
+              FormButton(text: "Iniciar sesión", onPressed: () => submit()),
+              SizedBox(height: screenHeight * .05),
+              NavigationLink(text: 'Registrarme', screen: RegisterScreen())
+            ],
+          ),
         ),
       ),
     );
